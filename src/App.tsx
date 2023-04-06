e2446dacfec1f1014905a3a9355be11a873b9e44
dev-luizf <luizfurtadomkt@hotmail.com>
2023-04-05 23:13:05 -0300

    feat: instala e configura tailwind e material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import './App.css';
+import './global.css';
 import { ToastProvider } from './hooks/useToast';
 
 function App() {
